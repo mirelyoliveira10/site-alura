056e0e6b93a66e06a1a5c825aa07256738f072e0
mirelyoliveira10 <143724722+mirelyoliveira10@users.noreply.github.com>
2025-03-31 12:25:47 +0000

    Instrutora

--- sitecriativo-main/script.js
+++ sitecriativo-main/script.js
@@ -1,11 +1,27 @@
-document.addEventListener('DOMContentLoaded', function(){
-const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-let tamanhoAtualFonte = 1;
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-aumentaFonteBotao.addEventListener('clic', function(){
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-})
+    })
 
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
 })
